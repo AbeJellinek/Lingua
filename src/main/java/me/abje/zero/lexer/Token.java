d334f067cec0c4ee9b34595710afd81500231031
Abe Jellinek <abe@newscenter.com>
2015-01-30 13:39:29 -0800

    Documented tokens and added a license

--- src/main/java/me/abje/zero/lexer/Token.java
+++ src/main/java/me/abje/zero/lexer/Token.java
@@ -22,11 +22,38 @@
 
 package me.abje.zero.lexer;
 
+/**
+ * A source token. Has a type, value, and position.
+ */
 public class Token {
+    /**
+     * This token's type.
+     */
     private Type type;
-    private String value;
-    private int line, column;
 
+    /**
+     * This token's value.
+     */
+    private String value;
+
+    /**
+     * This token's line number.
+     */
+    private int line;
+
+    /**
+     * This token's column number.
+     */
+    private int column;
+
+    /**
+     * Creates a new token with the given parameters.
+     *
+     * @param type   The token's type.
+     * @param value  The token's value.
+     * @param line   The token's line number.
+     * @param column The token's column number.
+     */
     public Token(Type type, String value, int line, int column) {
         this.type = type;
         this.value = value;
@@ -34,10 +61,16 @@ public class Token {
         this.column = column;
     }
 
+    /**
+     * Returns this token's type.
+     */
     public Type getType() {
         return type;
     }
 
+    /**
+     * Returns this token's value.
+     */
     public String getValue() {
         return value;
     }
@@ -47,30 +80,35 @@ public class Token {
         return type + " " + value;
     }
 
+    /**
+     * Returns whether this token's type equals the given type.
+     * @param type The type to compare to
+     * @return Whether this token's type equals the given type.
+     */
     public boolean is(Type type) {
         return this.type == type;
     }
 
+    /**
+     * Returns this token's line number.
+     */
     public int getLine() {
         return line;
     }
 
+    /**
+     * Returns this token's column number.
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * An enum of the possible token types.
+     */
     public static enum Type {
-        LINE,
-        LINE_CONTINUATION,
-        WHITESPACE,
-        NAME,
-        NUMBER,
-        DOT,
-        PLUS,
-        MINUS,
-        TILDE,
-        OPEN_PAREN,
-        CLOSE_PAREN,
-        TIMES, DIVIDE, POW, COMMA, EQ, OPEN_BRACE, CLOSE_BRACE, ARROW, LT, LTE, GT, GTE, TRUE, FALSE, EQEQ, NEQ, ANDAND, AND, OROR, OR, STRING, IF, ELSE, WHILE, DO, NULL, OPEN_BRACKET, CLOSE_BRACKET, CLASS, ANNOTATION, BANG
+        LINE, LINE_CONTINUATION, WHITESPACE, NAME, NUMBER, DOT, PLUS, MINUS, TILDE, OPEN_PAREN, CLOSE_PAREN, TIMES,
+        DIVIDE, POW, COMMA, EQ, OPEN_BRACE, CLOSE_BRACE, ARROW, LT, LTE, GT, GTE, TRUE, FALSE, EQEQ, NEQ, ANDAND, AND,
+        OROR, OR, STRING, IF, ELSE, WHILE, DO, NULL, OPEN_BRACKET, CLOSE_BRACKET, CLASS, ANNOTATION, BANG
     }
 }
