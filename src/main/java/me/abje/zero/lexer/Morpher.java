562ea796d674dd7aa1f0c21b79c8fd4b22707fa1
Abe Jellinek <abe@newscenter.com>
2015-01-30 13:32:49 -0800

    More documentation, copyright!

--- src/main/java/me/abje/zero/lexer/Morpher.java
+++ src/main/java/me/abje/zero/lexer/Morpher.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2015 Abe Jellinek
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package me.abje.zero.lexer;
 
 import me.abje.zero.Phase;
@@ -9,6 +31,9 @@ import me.abje.zero.Phase;
  * <p>
  * (Regarding the name: the Lexer generates "lexemes". This generates
  * "morphemes", the tokens that have semantic meaning.)
+ *
+ * @author Bob Nystrom
+ * @author Abe Jellinek
  */
 public class Morpher implements Phase<Void, Token> {
 
